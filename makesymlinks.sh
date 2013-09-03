01d53b54cdc2eecc322a656672c3a7fb2aef1190
Richard White <rwhite226@gmail.com>
2013-09-02 20:19:08 -0500

    small change to makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -4,12 +4,11 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
-########## Variables
-
+# Variables
 dir=~/.dotfiles                    # dotfiles directory
 files="xinitrc vimrc vim zshrc Xresources emacs emacs.d"    # list of files/folders to symlink in homedir
+gitacc=git@github.com:rwhite226/Dotfiles.git
 
-##########
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
@@ -21,9 +20,14 @@ for file in $files; do
 		echo "Creating symlink to $file in home directory."
 		        ln -s $dir/$file ~/.$file
 		done
+# Git add any changed files
 git add -A
+
+#Ask for and add a commit for git
 echo "Add commit"
 read commit
 git commit -m "$commit"
-git push git@github.com:rwhite226/Dotfiles.git
+
+# Push changes to gitacc
+git push $gitacc
 
